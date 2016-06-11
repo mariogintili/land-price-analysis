19a09591549096591903a3420a79a8e60758bcc2
mariogintili <mariojgintili@gmail.com>
2016-06-11 14:03:34 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to land-price-analysis
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/land_price_analysis/core_test.clj
@@ -0,0 +1,7 @@
+(ns land-price-analysis.core-test
+  (:require [clojure.test :refer :all]
+            [land-price-analysis.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
